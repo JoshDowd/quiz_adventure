a4e1a49b5871a7accca927951ff8428b65cf45fb
joshua dowd <jdowd18@VenerableBede.schools.sunderland.gov.uk>
2016-11-16 12:16:37 +0000

    i have completed quiz and i am finishing adventure at home

--- adventure.py
+++ adventure.py
@@ -1,9 +1,84 @@
-# An adventure game
+def World_Cup():
 
-def room1():
+  print("Your in the world Cup final, you are England against Spain, you are currently 1-0 down.")
+  print("you have the ball")
+  print("Do You pass it the")
+  print("A.left wing")
+  print("B.right wing")
+  decision = input("Decision: ")
 
-    ... make a game
+  if decision.lower() == "a":
+        left_wing()
 
+  if decision.lower() == "b":
+        right_wing()
+
+def left_wing():
+
+    print("You recieve the ball on the left wing")
+    print("Do you")
+    print("a.Dribble down the wing and cross into the box")
+    print("b.Take it into the middle")
+    decision = input("Decision: ")
+
+    if decision.lower() == "a":
+        box()
+
+    if decision.lower() == "b":
+        print("you are fouled, but the ref blows his full time whistle! Spain are the champs! You lose!")
+        print("Restarting...")
+        print("")
+        World_Cup()
+        
+def right_wing():
+
+    print("The ball is passed to the right wing")
+    print("Do you")
+    print("a.Dribble down the wing and cross into the box")
+    print("b.pass it to the keeper")
+    decision = input("Decision: ")
+
+    if decision.lower() == "a":
+        box()
+
+    if decision.lower() == "b":
+        keeper()
+
+
+
+def box():
+
+   print("The ball has been crossed into the box")
+   print("Do you")
+   print("a.head it at goal")
+   print("b.head it to you Vardy on the edge of the box")
+   decision = input("Decision: ")
+
+   if decision.lower() == "a":
+     print("Straight into the keepers arms, the whistle blows it's full time you lose")
+     print("Restarting...")
+     print("")
+     World_Cup()
+
+   if decision.lower() == "b":
+        ("Vardy strikes it, back off the net, it's full time and it's 1-1, penalties it is")
+        penalties()
+
+
+
+   
+   
+
+    
+
+    
+    
+
+    
+     
+    
+
+    
 
 
 
@@ -14,4 +89,4 @@ def room1():
 # Leave this at the bottom - it makes room1 run automatically when you
 # run your code.
 if __name__ == "__main__":
-    room1()
+    World_Cup()
--- quiz.py
+++ quiz.py
@@ -1,8 +1,94 @@
-# Our quiz!
+
+score = 0
+name = ""
 
 def quiz():
+    global name
 
-    ... make a quiz!
+    print("The Ultimate Football Quiz")
+    print("To play just input the letter you think the answer is")
+  
+    name = input("Name: ")
+    question1()
+    question2()
+    question3()
+    end()
+
+def question1():
+    global score
+    global name
+    
+    print("Question 1")
+    print("who is the youngest player on the current England senior national squad?")
+    print("A","Marcus Rashford")
+    print("B","Alex Oxlade Chamberlain")
+    print("C","Chris Smalling")
+    Answer = input("Answer, ")
+
+    if Answer.lower() == "a":
+          score = score + 1
+          print("Correct!")
+
+    else:
+          print("Incorrect!")
+          
+   
+
+def question2():
+    global score
+    global name
+    
+    print("Question 2")
+    print("Who is englands all time top scorer?")
+    print("A","Gary Lineker")
+    print("B","Wayne Rooney")
+    print("C","Bobby Charlton")
+    Answer = input("Answer, ")
+
+    if Answer.lower() == "b":
+          score = score + 1
+          print("Correct!")
+          
+    else:
+        print("Incorrect!")
+
+def question3():
+    global score
+    global name
+    
+    print("Question 3")
+    print("Who is Englands most capped player?")
+    print("A","Steven Gerrard")
+    print("B","David Beckham")
+    print("C","Peter Shilton")
+    Answer = input("Answer, ")
+
+          
+    if Answer.lower() == "c":
+          score = score + 1
+          print("Correct!")
+    else:
+        print("Incorrect!")
+
+def end():
+    global score
+    global name
+    
+    print("Your score is",score)
+
+    if score == 0:
+        print(name,"Your a nutter")
+
+    if score == 1:
+        print("You did OK",name)
+
+    if score == 2:
+        print("You did decent",name)
+
+    if score == 3:
+        print(name,"You're a genious")
+              
+
 
 
 
